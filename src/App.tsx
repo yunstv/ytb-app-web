0739762cf1b4924d2245dbb4d0fb6ae67de61b36
zy <yunstv@163.com>
2022-07-04 17:48:35 +0800

    feat: create app bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React or ytb-app-web
         </a>
       </header>
     </div>
